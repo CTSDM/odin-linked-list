57c1ca317ce3b11662209e2ae49beee7ecf59ed4
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-04-05 16:57:28 +0200

    Add insertAt(value, index) function

--- linkedList.mjs
+++ linkedList.mjs
@@ -46,8 +46,12 @@ export default class LinkedList {
     }
 
     at(index) {
-        if (index >= this.size() || index < 0)
+        if (index >= this.size() || index < 0) {
+            console.log(index)
+            console.log(this.#head);
+            console.log(this.size());
             throw new Error('index out of bounds of the linked list');
+        }
 
         let node = this.#head;
         while (index > 0) {
@@ -103,6 +107,39 @@ export default class LinkedList {
         }
         strToPrint += 'null';
         return strToPrint;
+    }
+
+    insertAt(value, index) {
+        // We can make use of the remove function
+        // And then insert a new one
+        // But, we would need to iterate again to find where we need to insert the new node
+
+        if (index === 0) {
+            this.prepend(value);
+            return;
+        }
+        let size = this.size();
+        if (index === size) {
+            this.append(value);
+            return;
+        }
+        if (index > this.size() || index < 0)
+            throw new Error('The given index is not within the bounds of the linked list.');
+        // We create the new Node 
+        const newNode = new Node(value);
+        if (index === 0)
+            this.#head = this.#head.nextNode;
+        if (index > 0) {
+            let node = this.at(index - 1);
+            let nextNode;
+            if (size - 1 - index > 0) {
+                nextNode = this.at(index + 1);
+            }
+            else
+                nextNode = null;
+            node.nextNode = newNode;
+            newNode.nextNode = nextNode;
+        }
     }
 
     remove(index) {
